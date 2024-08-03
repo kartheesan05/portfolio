7d49a6b76667070ba12a82d6e8449d65a5fb1705
Kartheesan69 <2022cs0138@svce.ac.in>
2024-08-03 18:04:26 +0530

    fixed card image redirect link

--- src/components/project/ProjectCard.jsx
+++ src/components/project/ProjectCard.jsx
@@ -6,17 +6,23 @@ function ProjectCard({ title, detailPage, coverImg, liveDemo, description }) {
   return (
     <div className="projectcard">
       <div className="imgwrapper">
-        <Link to={`/${detailPage}`}>
+        {detailPage ? (
+          <Link to={`/${detailPage}`}>
+            <img src={`/${coverImg}`} alt="" />
+          </Link>
+        ) : (
           <img src={`/${coverImg}`} alt="" />
-        </Link>
+        )}
       </div>
       <h4>{title}</h4>
       {/* <div className="description"></div> */}
       <p>{description}</p>
       <div className="projectcardbutton">
-        {detailPage && <Link to={`/${detailPage}`}>
-          <button>Learn More</button>
-        </Link>}
+        {detailPage && (
+          <Link to={`/${detailPage}`}>
+            <button>Learn More</button>
+          </Link>
+        )}
         <a href={liveDemo} target="_blank">
           <button>Live Demo</button>
         </a>
