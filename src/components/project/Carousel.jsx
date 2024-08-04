0cfee9ac707b354eaabb5b1f13d55875a91fc517
Kartheesan69 <2022cs0138@svce.ac.in>
2024-08-04 09:35:00 +0530

    restored Carousel.jsx

---
+++ src/components/project/Carousel.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+const Image = () => (
+  <div className="limitwidth">
+    <Carousel autoPlay={true} dynamicHeight={false} stopOnHover infiniteLoop>
+      <div>
+        <img src="https://raw.githubusercontent.com/kartheesan05/assets/main/portfolio/yc1.png" />
+      </div>
+      <div>
+        <img src="https://raw.githubusercontent.com/kartheesan05/assets/main/portfolio/yc2.png" />
+      </div>
+      <div>
+        <img src="https://raw.githubusercontent.com/kartheesan05/assets/main/portfolio/yc3.png" />
+      </div>
+      <div>
+        <img src="https://raw.githubusercontent.com/kartheesan05/assets/main/portfolio/yc4.png" />
+      </div>
+      <div>
+        <img src="https://raw.githubusercontent.com/kartheesan05/assets/main/portfolio/yc5.png" />
+      </div>
+    </Carousel>
+  </div>
+);
+export default Image;
