91bdbb2b66c82792c550a1824c0e7c1e8ae6b817
Kartheesan69 <2022cs0138@svce.ac.in>
2024-08-08 12:38:11 +0530

    test

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,7 +4,7 @@ import Sidebar from "./components/sidebar/Sidebar";
 import About from "./components/about/About";
 import Project from "./components/project/Project";
 import Contact from "./components/Contact/Contact";
-import Skills from "./components/skills/Skills";
+// import Skills from "./components/skills/Skills";
 
 function Home() {
   return (
