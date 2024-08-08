8c61d9521c0ff4c06ac2e977847332d0c25071a6
Kartheesan69 <2022cs0138@svce.ac.in>
2024-08-08 12:42:41 +0530

    removed unused import in Home

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,9 @@
 import "./app.scss";
 import Hero from "./components/hero/Hero";
-import Sidebar from "./components/sidebar/Sidebar";
+// import Sidebar from "./components/sidebar/Sidebar";
 import About from "./components/about/About";
 import Project from "./components/project/Project";
-import Contact from "./components/Contact/Contact";
+// import Contact from "./components/Contact/Contact";
 // import Skills from "./components/skills/Skills";
 
 function Home() {
